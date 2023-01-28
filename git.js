f4dcf7b9b8f704d424707d3652d005d3ec66634e
rabah-daguelou <lou.dag@yahoo.com>
2023-01-28 19:19:39 +0100

    essai

---
+++ git.js
@@ -0,0 +1,23 @@
+/*
+GitHub
+-	Créer un compte GitHub (e-mail+mot de passe)
+-	Installer git bash
+-	Configurer git dans le terminal git bash : Git config –global user.name ‘ nom utilisateur ‘
+                       Git config –global user.email adresse mail
+
+Etapes pour enregistrer dans github
+1-	Se connecter à son compte github
+2-	Créer un dépôt
+3-	Créer un dossier du projet
+4-	Accéder au dossier dans le terminal 
+5-	Initialiser le dépôt : git init (création d’un dossier .git caché)
+6-	Lier le dépôt avec le projet :
+7-	Suivre le(s) fichier(s) : $ git add GitHub.docx
+8-	Commiter  les fichiers suivis : 
+ 
+9-	Envoi du commit : 
+ 
+
+
+
+*/
